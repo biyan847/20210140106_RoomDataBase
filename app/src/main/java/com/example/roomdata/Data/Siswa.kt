42173c0b995a15be4aa94acbf8c9f36fa7bfc742
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-04 22:30:24 +0700

    create data class siswa

---
+++ app/src/main/java/com/example/roomdata/Data/Siswa.kt
@@ -0,0 +1,15 @@
+package com.example.roomdata.Data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telepon : String = "",
+
+    )
