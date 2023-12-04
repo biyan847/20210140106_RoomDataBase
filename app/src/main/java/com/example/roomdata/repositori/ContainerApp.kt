823b1acd0edf2edf971611d9b7770ab963212a33
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-04 22:43:25 +0700

    create package containerapp

--- app/src/main/java/com/example/roomdata/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdata/repositori/ContainerApp.kt
@@ -1,2 +1,13 @@
 package com.example.roomdata.repositori
 
+import android.content.Context
+import com.example.roomdata.Data.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy{
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
