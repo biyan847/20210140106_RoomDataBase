8507df60af7b462e05bfdc51b71feb6e55ecf528
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-06 13:43:58 +0700

    fungsi untuk menkonversi data input ke data dalam tabek sesuai jenis datanya

--- app/src/main/java/com/example/roomdata/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdata/model/EntryViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.roomdata.Data.Siswa
 import com.example.roomdata.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
@@ -17,9 +18,20 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() 
             nama.isNotBlank() && alamat.isNotBlank() && telepon.isNotBlank()
         }
     }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validationInput())
+    }
+
+    suspend fun SaveSiswa(){
+        if (validationInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+
 }
     /** status ui untuk siswa */
-data class  UIStateSiswa(
+data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
@@ -30,3 +42,20 @@ data class  DetailSiswa(
     val telepon: String ="",
 
     )
+/** fungsi untuk menkonversi data input ke data dalam tabek sesuai jenis datanya*/
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id =id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telepon,
+)
+fun Siswa.toUIStateSista(isEntryValid:Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id =id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telepon,
+)
