e0a15a98cf5e8477d37b3777815db72207d27225
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-06 14:45:38 +0700

    halamanentrii

---
+++ app/src/main/java/com/example/roomdata/ui/Halaman/HalamanEntry.kt
@@ -0,0 +1,10 @@
+package com.example.roomdata.ui.Halaman
+
+import com.example.roomdata.R
+import com.example.roomdata.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route= "item_entry"
+    override val titleRes= R.string.entry_siswa
+
+}
