b9097ec17984bc6d94ea9da741874e81bbcb7562
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-06 14:04:15 +0700

    fungsi ekstensi query untuk objek aplikasion dan unutk mengembaliakn instance AplikasiiSiswa

--- app/src/main/java/com/example/roomdata/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdata/model/PenyediaViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.roomdata.model
 
 import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdata.AplikasiSiswa
 
 object PenyediaViewModel {
 
@@ -15,3 +18,6 @@ object PenyediaViewModel {
         }
     }
 }
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
