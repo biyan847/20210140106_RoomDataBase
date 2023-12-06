7f64af9ac65d8b03a5a288f457df41ded81da253
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-06 13:21:51 +0700

    create class EntryViewModel in model package

---
+++ app/src/main/java/com/example/roomdata/model/EntryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.roomdata.model
+
+import androidx.lifecycle.ViewModel
+import com.example.roomdata.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+}
