7fdd0a5ecc96c2ab2ae30040ea6a196236351403
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-06 14:45:57 +0700

    halaman home

--- app/src/main/java/com/example/roomdata/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdata/model/PenyediaViewModel.kt
@@ -1,14 +1,13 @@
 package com.example.roomdata.model
 
-import android.text.Spannable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdata.AplikasiSiswa
 
-object PenyediaViewModel {
 
+object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             Homeviewmodel(aplikasiSiswa().container.repositoriSiswa)
@@ -17,7 +16,7 @@ object PenyediaViewModel {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
     }
-}
 
-fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
+}
+ fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
---
+++ app/src/main/java/com/example/roomdata/ui/Halaman/HalamanHome.kt
@@ -0,0 +1,10 @@
+package com.example.roomdata.ui.Halaman
+
+import com.example.roomdata.R
+import com.example.roomdata.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.app_name
+
+}
