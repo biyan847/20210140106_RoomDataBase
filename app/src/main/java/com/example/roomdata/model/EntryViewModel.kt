a479c7df6f49315f854890df935364cfa607f840
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-12-06 13:26:52 +0700

    status ui untuk siswa

--- app/src/main/java/com/example/roomdata/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdata/model/EntryViewModel.kt
@@ -3,6 +3,20 @@ package com.example.roomdata.model
 import androidx.lifecycle.ViewModel
 import com.example.roomdata.repositori.RepositoriSiswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+
 
 }
+    /** status ui untuk siswa */
+data class  UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+data class  DetailSiswa(
+    val id: Int =0,
+    val nama: String ="",
+    val alamat: String ="",
+    val telepon: String ="",
+
+    )
